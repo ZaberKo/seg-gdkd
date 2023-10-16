c67ffaa9a72927d8c0179a68c1ed00822b8cb511
zaber <20830726+ZaberKo@users.noreply.github.com>
2023-10-17 00:17:47 +0800

    change kd names

--- losses/distiller/__init__.py
+++ losses/distiller/__init__.py
@@ -4,12 +4,12 @@ from .dkd import DKD, DKDDIST
 from .gdkd import GDKD, GDKDDIST
 
 def get_criterion_distiller(name, *args, **kwargs):
-    return dict(
-        kd=KD,
-        kd_dist=KDDIST,
-        dkd=DKD,
-        dkd_dist=DKDDIST,
-        gdkd=GDKD,
-        gdkd_dist=GDKDDIST,
-        dist=DIST
-    )[name](*args, **kwargs)
+    return {
+        'kd': KD,
+        'kd-dist': KDDIST,
+        'dkd': DKD,
+        'dkd-dist': DKDDIST,
+        'gdkd': GDKD,
+        'gdkd-dist': GDKDDIST,
+        'dist': DIST
+    }[name](*args, **kwargs)
