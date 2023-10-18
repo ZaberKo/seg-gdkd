019f0536179b5b65f8ad53836abcb21fceb0ce18
zaber <20830726+ZaberKo@users.noreply.github.com>
2023-10-18 22:34:28 +0800

    add debug trainer

---
+++ dataset/datasets_debug.py
@@ -0,0 +1,58 @@
+import torch
+from torch.utils import data
+import os.path as osp
+import numpy as np
+import random
+import cv2
+cv2.setNumThreads(1)
+cv2.ocl.setUseOpenCL(False)
+
+from PIL import Image
+import os
+from torchvision import transforms
+
+from .datasets import CSTrainValSet
+
+class CSTrainValSetDebug(CSTrainValSet):
+
+    def __getitem__(self, index):
+        datafiles = self.files[index]
+        image = cv2.imread(datafiles["img"], cv2.IMREAD_COLOR)
+        label = cv2.imread(datafiles["label"], cv2.IMREAD_GRAYSCALE)
+
+        label = self.id2trainId(label)
+        
+        size = image.shape
+        img_path = datafiles["img"]
+        if self.is_scale:
+            image, label = self.generate_scale_label(image, label)
+        image = np.asarray(image, np.float32)
+        image = image - np.array([104.00698793, 116.66876762, 122.67891434])
+
+        img_h, img_w = label.shape
+        pad_h = max(self.crop_h - img_h, 0)
+        pad_w = max(self.crop_w - img_w, 0)
+        if pad_h > 0 or pad_w > 0:
+            img_pad = cv2.copyMakeBorder(image, 0, pad_h, 0, 
+                pad_w, cv2.BORDER_CONSTANT, 
+                value=(0.0, 0.0, 0.0))
+            label_pad = cv2.copyMakeBorder(label, 0, pad_h, 0, 
+                pad_w, cv2.BORDER_CONSTANT,
+                value=(self.ignore_label,))
+        else:
+            img_pad, label_pad = image, label
+
+        img_h, img_w = label_pad.shape
+        h_off = random.randint(0, img_h - self.crop_h)
+        w_off = random.randint(0, img_w - self.crop_w)
+        image = np.asarray(img_pad[h_off : h_off+self.crop_h, w_off : w_off+self.crop_w], np.float32)
+        label = np.asarray(label_pad[h_off : h_off+self.crop_h, w_off : w_off+self.crop_w], np.float32)
+        image = image.transpose((2, 0, 1))
+        if self.is_mirror:
+            flip = np.random.choice(2) * 2 - 1
+            image = image[:, :, ::flip]
+            label = label[:, ::flip]
+
+        return image.copy(), label.copy(), img_path
+
+
